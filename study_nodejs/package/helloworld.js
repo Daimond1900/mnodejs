a9c0381081a28a62495d335ef11c39e083f42a95
1900 <120622292@qq.com>
2017-07-01 07:15:44 +0800

    add

--- study_nodejs/package/helloworld.js
+++ study_nodejs/package/helloworld.js
@@ -1,6 +1,9 @@
-var http = require('http')
-var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('hello world');
-});
-server.listen(80);	
+// var http = require('http')
+// var server = http.createServer(function (req,res) {
+//     res.writeHead(200,{'Content-Type':'text/html'});
+//     res.end('<h1>Hello world</h1>');
+// });
+// server.listen(80);
+var mybuffer = new Buffer('1111111','base64');
+console.log(mybuffer);
+require('fs').writeFile('my.png',mybuffer);
