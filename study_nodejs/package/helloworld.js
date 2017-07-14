4d3c58b7b247a93223d490c06927f540f893ac8d
1900 <120622292@qq.com>
2017-07-14 09:04:43 +0800

    update

--- study_nodejs/package/helloworld.js
+++
@@ -1,9 +0,0 @@
-// var http = require('http')
-// var server = http.createServer(function (req,res) {
-//     res.writeHead(200,{'Content-Type':'text/html'});
-//     res.end('<h1>Hello world</h1>');
-// });
-// server.listen(80);
-var mybuffer = new Buffer('1111111','base64');
-console.log(mybuffer);
-require('fs').writeFile('my.png',mybuffer);
